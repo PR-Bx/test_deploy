fa9b2eaf7d9c8acedbfdf1e6d60b80e9b6cd262c
PR-Bx <peter.redshaw@bardsley-england.com>
2022-07-13 10:29:59 +0100

    build: add test_class and files required to distribute

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='test_package',
+    version='0.0.1',
+    author='Peter Redshaw',
+    author_email='peter.redshaw@bx-earth.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/PR-Bx/test_deploy',
+    license='None',
+    packages=['test_package'],
+    install_requires=['numpy'],
+)
---
+++ test_package/test_class.py
@@ -0,0 +1,9 @@
+class TestClass:
+    def __init__(self, input_1: float, input_2: float) -> None:
+        self.input_1 = input_1
+        self.input_2 = input_2
+
+    def test_method(self):
+        result = self.input_1 * self.input_2
+        result_str = str(result)
+        return "input_1 multiplied by input_2 = " + result_str
