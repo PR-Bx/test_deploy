d284d47f098d1130d0627f7e3621b30b5865d222
PR-Bx <peter.redshaw@bardsley-england.com>
2022-08-02 12:32:08 +0100

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="test_package",
-    version="0.0.1",
+    version="0.0.2",
     author="Peter Redshaw",
     author_email="peter.redshaw@bx-earth.com",
     description="Testing installation of Package",
